b81100ed2f8026bc9ce9065f2bd2e11039ca4961
Moulte <cedric.multeau@gmail.com>
2024-06-06 13:51:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,28 +12,11 @@ def front():
     return HTMLResponse(content=content)
 
 
-# @app.get("/etoro/stocks", response_class=JSONResponse)
-# async def get_etoro_stocks(api_key: str):
-#     response = requests.get(
-#         url=f"https://financialmodelingprep.com/api/v3/stock/full/real-time-price?apikey={api_key}"
-#     )
-#     decoded = response.json()
-#     from requests_html import AsyncHTMLSession
-
-#     session = AsyncHTMLSession()
-#     etoro_urls = []
-#     for symbol in [d["symbol"] for d in decoded][0:10]:
-#         etoro_urls.append( session.get(f"https://www.etoro.com/fr/markets/{symbol}"))
-
-
-#     availiable_symbol = []
-#     responses = await asyncio.gather(*etoro_urls)
-#     for response in responses:
-#         if response.status_code != 200:
-#             continue
-#         availiable_symbol.append(symbol)
-
-#     return []
+@app.get("/etoro/stocks", response_class=JSONResponse)
+async def get_etoro_stocks(api_key: str):
+    with open("etoro_symbols.json") as f:
+        data = json.load(f)
+    return data
 
 
 app.mount("/", StaticFiles(directory="build/web"), name="static")
